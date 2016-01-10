c01ac517bdfc2178e8123bc4a2ef3a0520f81c7a
Timmy <markcronin120@gmail.com>
2016-01-10 20:42:31 +0000

    application completed...

--- src/main/java/ie/cit/service/impl/AdminServiceImpl.java
+++ src/main/java/ie/cit/service/impl/AdminServiceImpl.java
@@ -6,10 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ie.cit.dao.PhysicianRepository;
-import ie.cit.domain.Patient;
 import ie.cit.domain.Physician;
 import ie.cit.service.AdminService;
-import ie.cit.service.MedicalService;
+
 
 @Service
 public class AdminServiceImpl implements AdminService{
